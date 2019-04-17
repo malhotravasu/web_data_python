218d042a51117695873b8d5708ffe497c41c4ef6
Vasu Malhotra <vmalhotra321@gmail.com>
2019-04-17 18:30:20 +0530

    Week6 Done! Course complete.

---
+++ week6/json_parse.py
@@ -0,0 +1,28 @@
+import json
+import urllib.request, urllib.parse, urllib.error
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter location: ')
+if len(url) < 1:
+    quit()
+
+print('Retrieving', url)
+uh = urllib.request.urlopen(url, context=ctx)
+
+data = uh.read()
+print('Retrieved', len(data), 'characters')
+print(data.decode())
+
+info = json.loads(data)
+comments = info['comments']
+
+sum = 0
+
+for comment in comments:
+    sum += comment['count']
+
+print("Sum: ", sum)
